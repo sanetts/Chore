f504cb6275b579584572155e307f0abdcdebf125
sanetts <sandra.nettey.sn@gmail.com>
2024-01-29 19:10:39 +0000

    add handle chore assignment

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function openAddChoreForm() {
     // Replace this with the actual logic to show a form for adding a new chore
     alert('Open form for adding a new chore');
 }
+
+
+function handleChoreAssignment(event) {
+    event.preventDefault();
+
+    alert('Chore assigned successfully!');
+
+}
+
+
+document.getElementById('choreAssignmentForm').addEventListener('submit', handleChoreAssignment);
